ce9478cea82f0f5dac5f73b774684170913626e1
Kent Kobi <kentkobi@hotmail.com>
2020-10-04 11:05:52 +1100

    updated server with api routers

--- server/server.js
+++ server/server.js
@@ -8,14 +8,16 @@ app.use(cors())
 
 require('./db-connect')
 const usersRouter = require('./controllers/users')
-const postsRouter = require('./controllers/posts')
+const scorecardsRouter = require('./controllers/scorecards')
+const scoresRouter = require('./controllers/scores')
 const middleware = require('./middleware')
 
 app.use(express.json()) 
 app.use(middleware.requestLogger)
 app.use(express.static('build'))
 app.use('/api/users', usersRouter)
-app.use('/api/posts', postsRouter)
+app.use('/api/scorecards', scorecardsRouter)
+app.use('/api/scores', scoresRouter)
 app.use(middleware.errorMiddleware)
 
 app.get('/*', function(req, res) {
