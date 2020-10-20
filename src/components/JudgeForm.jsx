34f1e4d43fadca6e4428c5512c2a4f76c539e3fc
Kent Kobi <kentkobi@hotmail.com>
2020-10-21 02:27:30 +1100

    added visual cues to judge form to acknowledge save

--- src/components/JudgeForm.jsx
+++ src/components/JudgeForm.jsx
@@ -4,10 +4,17 @@ import React, {useState} from 'react'
 const JudgeForm = ({judge, assignToGroup}) => {
     const existingGroup = (judge.user_metadata && judge.user_metadata.assigned) ? judge.user_metadata.assigned : ''
     const [group, setGroup] = useState(existingGroup)
+    const [btnLabel, setBtnLabel] = useState(group != '' ? "Assigned" : "Assign");
 
     const formHandler = (event) => {
         event.preventDefault()
         assignToGroup(judge.user_id, group)
+        setBtnLabel("Assigned")
+    }
+
+    const onGroupChange = (event) => {
+        setGroup(event.target.value)
+        setBtnLabel("Assign")
     }
   
     return (
@@ -16,9 +23,12 @@ const JudgeForm = ({judge, assignToGroup}) => {
                 className="form-control mb-2 mr-sm-2" 
                 name="group" 
                 defaultValue={group} 
-                onChange={e => setGroup(e.target.value)} 
+                onChange={e => onGroupChange(e)} 
                 placeholder="Judging for..." required/>
-            <button type="submit" className="btn btn-primary mb-2">Assign Judge</button>
+            <button type="submit" 
+                className='btn btn-primary mb-2'>
+                {btnLabel}
+            </button>
         </form>
     )
 }
